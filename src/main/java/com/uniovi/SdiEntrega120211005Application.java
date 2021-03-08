46fe06b0ec3a108ad864243ef049d41fa5d6f9b1
samuelmorenov <xamelsamuel@gmail.com>
2021-03-08 19:28:28 +0100

    Create project

---
+++ src/main/java/com/uniovi/SdiEntrega120211005Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega120211005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega120211005Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniovi/SdiEntrega120211005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiEntrega120211005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
