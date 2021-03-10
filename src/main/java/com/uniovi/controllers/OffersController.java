6f564fa1ce7373b70317def3354e433ffcf6a614
samuelmorenov <xamelsamuel@gmail.com>
2021-03-10 22:56:52 +0100

    Añadidas etiquetas que faltaban

--- src/main/java/com/uniovi/controllers/OffersController.java
+++ src/main/java/com/uniovi/controllers/OffersController.java
@@ -3,9 +3,11 @@ package com.uniovi.controllers;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import com.uniovi.services.OffersService;
 
+@Controller
 public class OffersController {
 
 	@Autowired
--- src/main/java/com/uniovi/services/OffersService.java
+++ src/main/java/com/uniovi/services/OffersService.java
@@ -4,9 +4,11 @@ import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.uniovi.repositories.OffersRepository;
 
+@Service
 public class OffersService {
 	@Autowired
 	private OffersRepository offersRepository;
