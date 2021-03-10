8659b97bd01faf3f3a4322e0f5dfc542e0dd5fd6
samuelmorenov <xamelsamuel@gmail.com>
2021-03-10 22:31:35 +0100

    Eliminadas opciones de añadir y detalles de usuarios, eliminada la paginacion de usuarios, eliminada la busqueda de usuarios

--- src/main/java/com/uniovi/controllers/UsersController.java
+++ src/main/java/com/uniovi/controllers/UsersController.java
@@ -1,27 +1,18 @@
 package com.uniovi.controllers;
 
 import java.security.Principal;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.uniovi.entities.User;
-import com.uniovi.services.RolesService;
 import com.uniovi.services.UsersService;
 
 @Controller
@@ -33,38 +24,6 @@ public class UsersController {
 	@Autowired
 	private UsersService usersService;
 
-	@RequestMapping(value = "/user/add")
-	public String user_add(Model model) {
-		model.addAttribute("rolesList", RolesService.getRoles());
-		return "user/add";
-	}
-
-	@RequestMapping(value = "/user/add", method = RequestMethod.POST)
-	public String user_add_POST(@ModelAttribute User user) {
-		usersService.addUser(user);
-		return "redirect:/user/list";
-	}
-
-	@RequestMapping("/user/details/{id}")
-	public String user_details_id(Model model, @PathVariable Long id) {
-		model.addAttribute("user", usersService.getUser(id));
-		return "user/details";
-	}
-
-	@RequestMapping(value = "/user/edit/{id}")
-	public String user_edit_id(Model model, @PathVariable Long id) {
-		User user = usersService.getUser(id);
-		model.addAttribute("user", user);
-		return "user/edit";
-	}
-
-	@RequestMapping(value = "/user/edit/{id}", method = RequestMethod.POST)
-	public String user_edit_id_POST(Model model, @PathVariable Long id, @ModelAttribute User user) {
-		user.setId(id);
-		usersService.addUser(user);
-		return "redirect:/user/details/" + id;
-	}
-
 	@RequestMapping("/user/delete/{id}")
 	public String user_delete_id(@PathVariable Long id) {
 		usersService.deleteUser(id);
@@ -72,38 +31,15 @@ public class UsersController {
 	}
 
 	@RequestMapping("/user/list")
-	public String user_list(Model model, Pageable pageable, Principal principal,
-			@RequestParam(value = "", required = false) String searchText) {
+	public String user_list(Model model, Principal principal) {
 
 		// Set active user
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		String email = auth.getName();
 		User activeUser = usersService.getUserByEmail(email);
 		httpSession.setAttribute("currentlyUser", activeUser);
-
-		// Paginacion y busqueda
-		Page<User> users = new PageImpl<User>(new LinkedList<User>());
-		if (searchText != null && !searchText.isEmpty()) {
-			users = usersService.searchByNameAndLastname(pageable, searchText);
-		} else {
-			users = usersService.getUsers(pageable);
-		}
-
-		// Añadir elementos al modelo
-
-		model.addAttribute("usersList", users.getContent());
-		model.addAttribute("page", users);
-
-		// Set lista de invitaciones
-		List<User> userList = null;//invitationsService.getUsersWithInvitation(activeUser); //TODO
-		model.addAttribute("usersWithInvitation", userList);
+		
+		model.addAttribute("usersList", usersService.getUsers());
 		return "user/list";
 	}
-
-	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
-	public String home(Model model, Pageable pageable, Principal principal,
-			@RequestParam(value = "", required = false) String searchText) {
-		return user_list(model, pageable, principal, searchText);
-	}
-
 }
