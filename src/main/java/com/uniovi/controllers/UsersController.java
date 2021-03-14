dc0f6d0551cb92ef77c8f798bf03afb242695012
samuelmorenov <xamelsamuel@gmail.com>
2021-03-14 15:18:04 +0100

    Ahora se borran correctamente los usuarios marcados en la vista del admin

--- src/main/java/com/uniovi/controllers/UsersController.java
+++ src/main/java/com/uniovi/controllers/UsersController.java
@@ -6,20 +6,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.uniovi.entities.User;
 
 @Controller
 public class UsersController extends UtilsController {
 
 	@RequestMapping(value = "/user/delete", method = RequestMethod.POST)
-	public String user_delete_id(Model model) {
-		// TODO: Obtener la lista de marcados y borrarlos
+	public String user_delete_id(Model model, @RequestParam(defaultValue = "false") String [] checkbox) {
+		for (int i = 0; i < checkbox.length; i++) {
+			User user = usersService.getUserByEmail(checkbox[i]);
+			usersService.deleteUser(user.getId());
+		}
 		return "redirect:/user/list";
 	}
 
 	@RequestMapping("/user/list")
 	public String user_list(Model model, Principal principal) {
 		// Set active user
-		this.setActiveUser(model);
+		User activeUser = this.setActiveUser(model);
 		
 		model.addAttribute("usersList", usersService.getUsers());
 		return "user/list";
